87460eac3a973be76c188610cc962fddb39b44be
MDCDEV <me@mdcdev.me>
2022-07-19 20:05:20 -0500

    Little Changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import bodyParser from "body-parser";
 import express from "express";
+import axios from "axios";
+
+if(process.env.ENV !== "production") {
+  require("dotenv").config();
+}
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -10,10 +15,63 @@ app.use(bodyParser.text({ type: "text/html" }));
 
 app.post("/weebhook", async (req, res) => {
   const data = req.body;
-  console.log(data);
-  res.sendStatus(200);
+  const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL || "";
+
+  if(!DISCORD_WEBHOOK_URL || DISCORD_WEBHOOK_URL === "") res.status(500).send("No Discord Webhook URL");
+
+  if(!data.status || !data.service) res.status(500).send("No status or service");
+
+
+  interface embed {
+    title: string;
+    description?: string;
+    fields?: Array<{
+      name: string;
+      value: string;
+    }>;
+    timestamp?: string;
+    author?: {
+      name: string;
+      icon_url?: string;
+      url?: string;
+    };
+    color?: number;
+  }
+
+  let embed = <embed>{
+    title: `${data.name}`,
+    fields: [
+      {
+        name: "Status",
+        value: `${data.status}`,
+      },
+      {
+        name: "Service",
+        value: `${data.service.name}`,
+      }
+    ],
+    timestamp: data.timestamp,
+    author: {
+      name: data.deployment.creator.name,
+      icon_url: data.deployment.creator.avatar
+    }
+  }
+
+  if (data.status === "SUCCESS") embed = { ...embed, color: 0x00ff00 };
+  if (data.status === "BUILDING") embed = { ...embed, color: 0xffff00 };
+  if (data.status === "DEPLOYING") embed = { ...embed, color: 0xffff00 };
+  if (data.status === "CRASHED") embed = { ...embed, color: 0xff0000 };
+
+  const payload = {
+    embeds: [embed]
+  }
+
+  await axios.post(DISCORD_WEBHOOK_URL, payload);
+
+  res.status(200).send("OK");
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening on port ${port}`);
 });
